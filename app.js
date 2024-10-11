0b9097003a9c7aa2be466fa821ee849a24c64511
Gowtham Raju <gowthamraju239@gmail.com>
2024-10-11 18:51:14 +0530

    add recipe object
    - add imagesrc to the recipe object
    - add title to the recpe object
    - add ingredients list as a array to the object

---
+++ app.js
@@ -0,0 +1,12 @@
+const recipe = {
+    title : "Tomato Pasta",
+    imageSrc : "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/recipe-img.png",
+    ingredients: ["Pasta", "Oil", "Onions", "Salt", "Tomato Pasta Sauce", "Cheese"]
+}
+
+const recipeHeading = document.getElementById("recipeHeading");
+recipeHeading.textContent = recipe.title;
+
+const imageContainer = document.getElementById("imageContainer");
+const image = document.getElementsByTagName("img");
+image.src = recipe.imageSrc;
